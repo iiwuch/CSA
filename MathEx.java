e7bd5fc269a0dd4a55bd2969d8f8ae9c09d5a143
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2023-09-26 10:23:43 -0400

    Math commit

--- MathEx.java
+++ MathEx.java
@@ -1,3 +1,24 @@
-public MathEx Main {
-    public static void
+public class MathEx {
+    public static void main(String [] args){
+        System.out.println(swap(132));
+        System.out.println(swap(276));
+        System.out.println(swap(304));
+        System.out.println(timeLeft)
+    }
+    
+    public static int swap(int y){
+        int onesPlace = y%10;
+        int tensPlace = y%100/10;
+        int hundredsPlace = y/100;
+        int result = hundredsPlace*100 + onesPlace*10 + tensPlace;
+        return result;
+    }
+
+    public static int timeLeft(int curHour, int curMin, int depHour, int, depMin){
+        int curHour =
+        int curMin =
+        int depHour =
+        int depMin =
+
+    }
 }
---
+++ Random.java
@@ -0,0 +1,8 @@
+public class Random {
+    public static void
+    System.out.println(x);
+
+    x=Math.random()*20;
+    System.out.println(x);
+    int a = 
+}
