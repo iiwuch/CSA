617780ec69889c2d66b95a39d73bb098d518d056
iiwuch <iwuchukwuiwuchukwu@billericak12.com>
2023-10-30 10:24:10 -0400

    conditional commit

---
+++ conditionals.java
@@ -0,0 +1,21 @@
+public class conditionals {
+
+    public boolean chimpTrouble(boolean aSmile, boolean bSmile){
+        if aSmile == bSmile{
+            return true
+        else
+            return false
+        }
+    }
+    public negPos(int a, int b){
+        
+    }
+    public static void main(String [] args){
+        chimpTrouble(true, true);
+        chimpTrouble (false, false);
+        chimpTrouble (true, false);
+        chimpTrouble (false, true);
+        
+
+    }
+}
