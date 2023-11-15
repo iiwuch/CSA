de3437c25eb28d1ed7e4d1b2b6a9c129a8802a55
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2023-11-14 21:02:04 -0500

    spinner commit

--- TwoSpinners.java
+++ TwoSpinners.java
@@ -6,24 +6,62 @@ public class TwoSpinners {
     * inclusive. Complete the spin method below by assigning
     * this random integer to result.
     */
+    private int roll1 = 0;
+    private int rollc = 0;
+    private int points = 0;
+
     public int spin(int min, int max){
-        int result=0;
-        result =(int)(Math.random()*max + min);
+        int result = 0;
+        if (min < max){
+        result = (int)(Math.random()*max + min);
         return result;
         }
-   
+        else return result;
+    }
     /** Simulates one round of the game as described in part (b).
     */
     public void playRound(){
-        if (p1.spin(1,10) >= comp.spin(2,8));
-        // add code here    	
+        TwoSpinners p1 = new TwoSpinners();
+        TwoSpinners comp = new TwoSpinners();
+        
+        roll1 = p1.spin(1,10);
+        rollc = comp.spin(2,8);
+        
+        if (rollc < roll1){
+            points = roll1 - rollc;
+            System.out.println("You win! " + points + " points");
+        }
+
+        if (rollc > roll1){
+            points = roll1 - rollc;
+            System.out.println("You Lose! " + points + " points");
+        }
+
+        if (rollc == roll1){
+            roll1 = p1.spin(1,10);
+            rollc = comp.spin(2,8);
+            
+                if (rollc < roll1){
+                    points = roll1 - rollc;
+                    System.out.println("You win! " + points + " points");
+            }
+
+                if (rollc > roll1){
+                    points = roll1 - rollc;
+                    System.out.println("You Lose! " + points + " points");
+                }
+
+                if (rollc == roll1) {
+                    points = roll1 - rollc;
+                    System.out.println("Tie! " + points + " points");
+                    }
+
+                }
     }
+        // add code here    	
    
     public static void main(String[] args) {
         TwoSpinners ds = new TwoSpinners();
-        TwoSpinners comp = new TwoSpinners();
-        TwoSpinners p1 = new TwoSpinners();
-        System.out.println(ds.spin(1,9));
         for(int i = 0; i < 10; i++){    //This will save you time by running playground 10 times
             ds.playRound();
         }
