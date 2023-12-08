710f15837d0bcdaefc7d6c3f2341be786eb27c55
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2023-12-08 10:21:51 -0500

    comit

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,51 @@
 public class Factorial {
 
-    public int CalcFactorial(int n){
-        int product = 1;
+    public long CalcFactorial(int n){
+        long product = 1;
         int i = 1;
-        while(1 < n){
+        while(i < n){
             i++;
             product *= i;
         }
         return product;
     }
 
+    public double CalcE(){ //fix decimal rounding
+
+        double e = 1.0;
+        double olde = 0.0;
+        int i = 0;
+        while(e - olde > 0.001){ 
+            i++;
+            olde = e;
+            e += 1.0/(CalcFactorial(i));
+        } 
+        return e;
+    }
+
+    public double calcEX(int x){
+        double e = 1.0;
+        double olde = 0.0;
+        int i = 0;
+        while(e-olde > 0.001){
+            i++;
+            olde = e;
+            e += (Math.pow(x,i)/CalcFactorial(i));
+        }
+        return e;
+    }
+
     public static void main(String [] args){
         Factorial test = new Factorial();
-        System.out.println(test.CalcFactorial(4));
-        System.out.println(test.CalcFactorial(8));
+        int n = 1;
+        for (n=1; n <= 20; n++){
+            System.out.println(test.CalcFactorial(n));
+        }
+        System.out.println();
+        System.out.printf("e is %2.3f\n", test.CalcE());
+        System.out.println();
+        for (n=1; n <= 5; n++){
+            System.out.printf("e is %2.3f\n", test.calcEX(n));
+        }
     }
 }
