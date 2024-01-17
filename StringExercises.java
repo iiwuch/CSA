a26bf062d80cdd956425a65761ab4ce32e6f4121
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2024-01-16 21:57:40 -0500

    comit

--- StringExercises.java
+++ StringExercises.java
@@ -15,12 +15,30 @@ public class StringExercises {
     }
 
     public static String negative(String n){
-        n = n.replace(0,2);
-        n = n.replace(1,3);
-        n = n.replace(2,1);
-        n = n.replace(3,0);
+        n = n.replace("0","2");
+        n = n.replace("1","3");
+        n = n.replace("2","1");
+        n = n.replace("3","0");
         return n;
     }
+
+    public static String convertDate(String dateA){
+        String dateE;
+        dateE = dateA.substring(3,6) + dateA.substring(0,3) + dateA.substring(6,10);
+        return dateE;
+    }
+
+    // public static String convertDate2(String dateA){
+    //     String day = if(dateA.charAt(dateA.substring(0,2));
+    //     String month =  ;
+    //     String year;
+    //     return day + month + year;
+    // }
+
+    // public static boolean startsWith(String s, String prefix){
+    //     int pre = prefix.length();
+    //     return (s.substring(0,pre) == prefix);
+    // }
 
     public static void main(String[] args) {
         System.out.println(scroll("Hello World"));
@@ -29,9 +47,9 @@ public class StringExercises {
         System.out.println(convertName(" Reubenstein, Lori Renee "));
         System.out.println(convertName("Biden,Joe"));
         System.out.println(convertName("the Clown, Bozo"));
-        // System.out.println(negative("0010111001"));
-        // System.out.println(negative("11111111"));
-        // System.out.println("04/20/2014 becomes " + dateString("04/20/2014"));
+        System.out.println(negative("0010111001"));
+        System.out.println(negative("11111111"));
+        System.out.println("04/20/2014 becomes " + convertDate("04/20/2014"));
         // System.out.println("04/20/2014 becomes" + dateString2("04/20/2014"));
         // System.out.println("4/20/2014 becomes" + dateString2("4/20/2014"));
         // System.out.println("04/2/2014 becomes" + dateString2("04/2/2014"));
