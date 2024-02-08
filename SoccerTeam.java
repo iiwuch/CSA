fa6143bd319e8476dbbcc7397f12dd4330145644
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2024-02-08 10:33:37 -0500

    comit

---
+++ SoccerTeam.java
@@ -0,0 +1,14 @@
+public class SoccerTeam {
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public SoccerTeam(int win, int los, int tie){
+        wins = win;
+        losses = los;
+        ties = tie;
+    }
+
+    
+
+}
