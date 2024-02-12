e118fe7b38f6196eadf19d3f82645e5ef9fd0e82
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2024-02-12 10:24:52 -0500

    comit

--- Complex.java
+++ Complex.java
@@ -1,3 +1,56 @@
 public class Complex {
     
+    private double a = 0;
+    private double b = 0;
+
+    public Complex(double a1){
+        this(a1, 0.0);
+    }
+
+    public Complex(double a1, double b1){
+        a = a1;
+        b = b1;
+    }
+
+    public double abs(){
+        return (Math.sqrt(a*a + b*b));
+    }
+
+    public Complex add(Complex other){
+        double real = this.a + other.a;
+        double imaginary = this.b + other.b;
+        Complex num = new Complex(real,imaginary);
+        return num;
+    }
+
+    public Complex multiply(Complex other){
+        double a = this.a
+        double b = 
+        Complex product = new product()
+    }
+
+    public String toString(){
+        String string = this.a + " + " + this.b + "i";
+        return string;
+    }
+
+    public static void main(String [] args){
+        Complex test1 = new Complex(1,2);
+        Complex test2 = new Complex(3,-4);
+        Complex test3 = new Complex(-3,-4);
+        Complex test4 = new Complex(0.0,4);
+        Complex test5 = new Complex(-3,0.0);
+        Complex test6 = new Complex(0.0,-3);
+        Complex test1a2 = test1.add(test2);
+        Complex test3a4 = test3.add(test4);
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
+        System.out.println(test5);
+        System.out.println(test6);
+        System.out.println(test1a2);
+        System.out.println(test3a4);
+        System.out.println(test3.abs());
+        }    
 }
--- SoccerTeam.java
+++ SoccerTeam.java
@@ -2,6 +2,8 @@ public class SoccerTeam {
     private int wins;
     private int losses;
     private int ties;
+    private static int gamesp;
+    private static int points;
 
     public SoccerTeam(int win, int los, int tie){
         wins = win;
@@ -9,6 +11,39 @@ public class SoccerTeam {
         ties = tie;
     }
 
-    
+    public void played(SoccerTeam other, int myScore, int otherScore){
+        if(myScore > otherScore){
+            this.wins ++;
+            other.losses ++;
 
+        }
+        if(myScore < otherScore){
+            this.losses++;
+            other.wins++;
+        }
+        else{
+            this.ties++;
+            other.ties++;
+        }
+        this.gamesp++;
+        other.gamesp++;
+    }
+    
+    public int getPoints(SoccerTeam other){
+        return wins*3 + ties;
+    }
+
+    public void reset(SoccerTeam other){
+        wins = 0;
+        ties = 0;
+        losses = 0;
+    }
+
+    public static startTournament(){
+        
+    }
+
+    public static void main(String[] args){
+
+    }
 }
