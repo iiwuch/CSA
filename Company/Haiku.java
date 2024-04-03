b23a0d163dbc2e3969517a9d9c6713beb9a9df60
iiwuch <ifechukwuiwuchukwu@billericak12.com>
2024-04-03 10:22:14 -0400

    comit

---
+++ Company/Haiku.java
@@ -0,0 +1,14 @@
+package Company;
+
+public class Haiku extends Poem {
+
+    public Haiku(){
+        super(3);
+    }
+
+    public void printRhythm(){
+        System.out.println("ra-ta-ta-la-sa");
+        System.out.println("ta-sa-ta-ra-ta-la-sa");
+        System.out.println("ra-ta-ta-la-sa");
+    }
+}
